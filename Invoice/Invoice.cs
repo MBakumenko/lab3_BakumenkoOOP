ced409a680d60c91ee891100247a5c6cc4195f95
Maxym <maxbakumenko@ukr.net>
2022-10-10 22:49:39 +0300

    Invoice commit

---
+++ Invoice/Invoice.cs
@@ -0,0 +1,26 @@
+﻿namespace Invoice
+{
+    public class Invoice
+    {
+        private readonly int _account;
+        private readonly string _provider;
+        private readonly string _customer;
+
+        private string _article;
+        private int _quantity;
+
+        public Invoice(int account, string provider, string customer, string article, int quantity)
+        {
+            _account = account;
+            _provider = provider;
+            _customer = customer;
+            _article = article;
+            _quantity = quantity;
+        }
+
+        public double GetSum(bool UsePdv = true)
+        {
+            return UsePdv ? _account * _quantity * 1.2 : _quantity * _account;
+        }
+    }
+}
