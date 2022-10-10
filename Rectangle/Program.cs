636174a2c4605e137031d9d9e6fb412259a276a6
Maxym <maxbakumenko@ukr.net>
2022-10-10 19:48:19 +0300

    Rectangle commit

---
+++ Rectangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        static void Warning()
+        {
+            throw new ArgumentException("Сторона не може бути меншим за 0");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            double side1, side2;
+            Console.WriteLine("Введіть довжину першої сторони");
+            if (!double.TryParse(Console.ReadLine(), out side1) || side1 < 0)
+            {
+                Warning();
+            }
+            Console.WriteLine("Введіть довжину другої сторони");
+            if (!double.TryParse(Console.ReadLine(), out side2) || side2 < 0)
+            {
+                Warning();
+            }
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площа: {rectangle.Area}");
+            Console.WriteLine($"Периметр: {rectangle.Perimeter}");
+        }
+    }
+}
---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace Rectangle
+{
+    public class Rectangle
+    {
+        private double _side1;
+        private double _side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this._side1 = side1;
+            this._side2 = side2;
+        }
+
+        private double AreaCalculator() => _side1 * _side2;
+        private double PerimeterCalculator() => 2 * (_side1 + _side2);
+
+        public double Area => AreaCalculator();
+        public double Perimeter => PerimeterCalculator();
+    }
+}
